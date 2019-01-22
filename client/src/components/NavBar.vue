197fdbbf54652e94c749542a09bb2422d92fd376
Lucas Luna Souza <lucas.lunasouza@mail.utoronto.ca>
2019-01-22 00:42:30 -0500

    Header routing

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="nav-bar">
+    <NavButton text="About" />
+    <NavButton text="Pricing" />
+    <NavButton text="Sign Up" border />
+    <NavButton text="Login" />
+    <div class="mobile-nav-bar">
+      <div class="hamburger" />
+      <div class="hamburger" />
+      <div class="hamburger" />
+    </div>
+  </div>
+</template>
+
+<script>
+import NavButton from '@/components/NavButton'
+
+export default {
+  name: 'NavBar',
+  components: {
+    NavButton
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../assets/styles/colors";
+  @import "../assets/styles/functions";
+
+  .mobile-nav-bar {
+    display: none;
+  }
+  .nav-bar {
+    display: flex;
+    width: 400px;
+    justify-content: space-evenly;
+  }
+  #sign-up {
+    border: 1px solid color('text');
+    border-radius: 3px;
+  }
+  #sign-up:hover {
+    background: color('text');
+    color: color('background');
+    text-decoration: none;
+  }
+  .hamburger {
+    width: 20px;
+    height: 3px;
+    background-color: color('text');
+    margin: 5px 7px;
+  }
+
+  @media only screen and (max-width: 700px){
+    .nav-bar {
+      justify-content: flex-end;
+    }
+    .mobile-nav-bar {
+      cursor: pointer;
+      border: 1px solid color('text');
+      border-radius: 3px;
+      display: inline;
+      padding: 3px;
+    }
+  }
+</style>
--- client/src/views/DashboardList.vue
+++ client/src/views/DashboardList.vue
@@ -63,6 +63,7 @@ export default {
   }
 }
 </script>
+
 <style lang="scss" scoped>
 .table-wrap {
   width: 60%;
