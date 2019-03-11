cce5fd7a37f99d275a3bba71d0cb21a1b5bfc7be
Lucas Luna Souza <lucas.lunasouza@mail.utoronto.ca>
2019-03-11 01:24:06 -0400

    User API

--- user_service/src/schema.js
+++ user_service/src/schema.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-  name: String,
-  username: String,
-  email: String
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true},
+  name: { type: String, required: true},
+  email: { type: String, required: true},
 })
 
 const User = mongoose.model('User', UserSchema)
