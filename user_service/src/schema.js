4798037c01f1d0f02e5ce2e80399e1ff3babac28
Lucas Luna Souza <lucas.lunasouza@mail.utoronto.ca>
2019-03-10 23:50:30 -0400

    Set up passport strategies

--- user_service/src/schema.js
+++ user_service/src/schema.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
+  name: String,
+  username: String,
+  email: String
 })
 
 const User = mongoose.model('User', UserSchema)
