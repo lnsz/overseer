403bdfb7cf1ae08269bc65c21b3e48002dfbbfc8
Lucas Luna Souza <lucas.lunasouza@mail.utoronto.ca>
2019-03-25 02:15:18 -0400

    Refactored docker build process

--- client/docker/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ -z "$(ls -A ./node_modules)" ]; then
-  echo "Installing dependencies (this will only run the first time your container is created. If 'node_modules' already exists, this will be skipped for subsequent cointainers)..."
-  npm install --no-package-lock
-else
-  echo "Skipping 'npm install' because 'node_modules' already exists. Delete 'node_modules' and restart the container if you want to reinstall binaries."
-fi
-
-exec "$@";
