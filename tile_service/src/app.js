effe175102dfd646d30133d94b57f8caf8d790cf
Lucas Luna Souza <lucas.lunasouza@mail.utoronto.ca>
2019-04-13 23:40:31 -0400

    Interal ports for communication between services

--- tile_service/src/app.js
+++ tile_service/src/app.js
@@ -2,26 +2,24 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-
-const app = express()
-app.use(morgan('combined'))
-app.use(bodyParser.json())
-app.use(cors())
-
-// DB Setup
 const mongoose = require('mongoose')
 
-const DATABASE_URL = process.env.DATABASE_URL || 'http://localhost'
-mongoose.connect(`mongodb://${DATABASE_URL}`)
-
+// DB Setup
+mongoose.connect(`mongodb://${process.env.DATABASE_URL}`)
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error'))
 db.once('open', () => {
   console.log('Connection Succeeded')
 })
 
+// Express Setup
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
 app.use('/api', require('./router'))
 
-app.listen(process.env.PORT || 8000, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server running on ${process.env.SERVICE_URL}`)
 })
